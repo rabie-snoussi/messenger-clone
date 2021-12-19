96d61dd33729beb0b85d10346b4e44154e050fa9
Rabie Snoussi <rabie.snoussi.dev@gmail.com>
2021-12-19 15:42:36 +0100

    userId fix in 'message.model'

--- server/src/model/message.model.ts
+++ server/src/model/message.model.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { UserDocument } from './user.model';
 
 export interface MessageDocument extends mongoose.Document {
-  user: UserDocument;
+  userId: UserDocument;
   message: string;
   createdAt: Date;
   updatedAt: Date;
